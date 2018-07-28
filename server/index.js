695d24a6041247a88d71ab96ce33cc9413207477
Debalina Mukherjee <dmukherjee@users.noreply.github.com>
2018-07-28 12:12:36 -0700

    Feat/implement api endpoint to get listing (#7)
    * (chore) add mocked data
    
    * (feat) api endpoint to get listing by location
    
    * (refactor) get location using req.params

--- server/index.js
+++ server/index.js
@@ -1,7 +1,16 @@
 const express = require('express');
 
+const Listing = require('../data/mockedDataSearch');
+
 const app = express();
 const PORT = 3001;
+
+app.get('/api/listing/:location', (req, res) => {
+  const queryString = req.params.location.toLowerCase();
+  const results = Listing.listings.filter(listing =>
+    listing.unitAddress.toLowerCase().includes(decodeURI(queryString)));
+  res.status(200).send(results);
+});
 
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);

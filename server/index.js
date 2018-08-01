47ac5ed642bc702b214779313b5ef1a548c7fce6
Debalina Mukherjee <dmukherjee@users.noreply.github.com>
2018-08-01 00:41:11 -0700

    (refactor) generate mocked data with faker (#10)
    * (chore) add faker has a dependency
    
    * (feat) generate mocked data with faker
    
    * (refactor) update get request to retrieve search result based on mocked data

--- server/index.js
+++ server/index.js
@@ -1,13 +1,13 @@
 const express = require('express');
 
-const Listing = require('../data/mockedDataSearch');
+const Listings = require('../data/mockedDataSearch');
 
 const app = express();
 const PORT = 3001;
 
 app.get('/api/listing/:location', (req, res) => {
   const queryString = req.params.location.toLowerCase();
-  const results = Listing.listings.filter(listing =>
+  const results = Listings.filter(listing =>
     listing.unitAddress.toLowerCase().includes(decodeURI(queryString)));
   res.status(200).send(results);
 });

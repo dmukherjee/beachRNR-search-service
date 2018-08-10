90273adfad857471f7cdd5cda9c8f68fed3299cc
Debalina Mukherjee <dmukherjee@users.noreply.github.com>
2018-08-09 23:47:02 -0700

    (feat) index 10M data in batches of 10K (#18)
    * (chore) rename data generator file
    
    * (feat) index data in batches of 500
    
    * update data generator to generate 1M listings
    
    * (refactor) wrap data generator in a function to enable indexing data in chunks
    
    * (feat) add async method to load data in chunks
    
    * (chore) comment console.log statement

--- data/dataGenerator.js
+++ data/dataGenerator.js
@@ -3,29 +3,33 @@ const faker = require('faker');
 const cityWithState = ['New York, NY', 'New Jersey, NJ', 'San Francisco, CA',
   'Chicago, IL', 'Seattle, WA', 'Boston, MA', 'Los Angeles, CA'];
 
-const listings = [];
-for (let i = 2912000; i < 3012100; i += 1) {
-  const randomCityWithState = cityWithState[Math.floor(Math.random() * cityWithState.length)];
-  const listing = {
-    id: i,
-    unitName: `${faker.address.cityPrefix()} ${faker.address.citySuffix()} ${faker.address.streetSuffix()}`,
-    unitImage: `https://s3.us-east-2.amazonaws.com/bnbsearch/images/${faker.random.number({ min: 1, max: 35 })}.jpg`,
-    hostId: faker.random.number(),
-    hostName: faker.name.firstName(),
-    isSuprhost: faker.random.boolean(),
-    unitAddress: `${faker.address.streetAddress()}, ${randomCityWithState} ${faker.address.zipCode('#####')}, United States`,
-    city: `${randomCityWithState.split(',')[0]}`,
-    country_code: 'US',
-    property_type: faker.random.arrayElement(['House', 'Apartment', 'Condominium', 'Villa', 'Bed & Breakfast', 'Townhouse']),
-    room_type: faker.random.arrayElement(['Private room', 'Entire home/apt', 'shared room']),
-    beds: faker.random.number({ min: 1, max: 5 }),
-    unitPrice: `$${faker.random.number({ min: 70, max: 500 })}`,
-    priceModifier: 'per night',
-    numberOfReviews: faker.random.number({ min: 50, max: 2000 }),
-    reviewScoresRating: faker.random.arrayElement([3, 3.5, 4, 4.5, 5]),
-    freeCancellation: faker.random.boolean()
-  };
-  listings.push(listing);
-}
+const generateData = (index) => {
+  const listings = [];
+  for (let i = index; i < index + 10000; i += 1) {
+    const randomCityWithState = cityWithState[Math.floor(Math.random() * cityWithState.length)];
+    const listing = {
+      id: i,
+      unitName: `${faker.address.cityPrefix()} ${faker.address.citySuffix()} ${faker.address.streetSuffix()}`,
+      unitImage: `https://s3.us-east-2.amazonaws.com/bnbsearch/images/${faker.random.number({ min: 1, max: 35 })}.jpg`,
+      hostId: faker.random.number(),
+      hostName: faker.name.firstName(),
+      isSuprhost: faker.random.boolean(),
+      unitAddress: `${faker.address.streetAddress()}, ${randomCityWithState} ${faker.address.zipCode('#####')}, United States`,
+      city: `${randomCityWithState.split(',')[0]}`,
+      country_code: 'US',
+      property_type: faker.random.arrayElement(['House', 'Apartment', 'Condominium', 'Villa', 'Bed & Breakfast', 'Townhouse']),
+      room_type: faker.random.arrayElement(['Private room', 'Entire home/apt', 'shared room']),
+      beds: faker.random.number({ min: 1, max: 5 }),
+      unitPrice: `$${faker.random.number({ min: 70, max: 500 })}`,
+      priceModifier: 'per night',
+      numberOfReviews: faker.random.number({ min: 50, max: 2000 }),
+      reviewScoresRating: faker.random.arrayElement([3, 3.5, 4, 4.5, 5]),
+      freeCancellation: faker.random.boolean()
+    };
+    listings.push(listing);
+  }
+  return listings;
+};
 
-module.exports = listings;
+module.exports = { generateData };
+
--- server/loadData.js
+++ server/loadData.js
@@ -1,26 +1,39 @@
 const esConnection = require('./connection');
 const listings = require('../data/dataGenerator');
+const totalNumberOfRecords = 10000000
+let startIndex = 2912000
+
+let endIndex = startIndex + totalNumberOfRecords;
 
 async function insertListingData(data) {
   let bulkOps = [];
-  await esConnection.checkConnection();
-  await esConnection.resetIndex();
   for (let i = 0; i < data.length; i++) {
-    // console.log('data[i].id', data[i].id);
     bulkOps.push({ index: { _index: esConnection.index, _type: esConnection.type, _id: data[i].id }},
       data[i]
     );
-    console.log(`Adding ${data[i].id} to array`);
-    console.log(bulkOps);
+    // console.log(`Adding ${data[i].id} to array`);
+    // console.log(bulkOps);
     if (i > 0 && i % 500 === 0) {
       await esConnection.client.bulk({ body: bulkOps });
       bulkOps = [];
-      console.log(`Indexed listings ${i - 499} - ${i}`);
+      // console.log(`Indexed listings ${i - 499} - ${i}`);
     }
   }
 
   await esConnection.client.bulk({ body: bulkOps });
-  console.log(`Indexed data ${data.length - (bulkOps.length / 2)} - ${data.length}\n\n\n`);
+  // console.log(`Indexed data ${data.length - (bulkOps.length / 2)} - ${data.length}\n\n\n`);
 }
 
-insertListingData(listings);
+async function indexData() {
+  await esConnection.resetIndex();
+  while (startIndex < endIndex) {
+    await esConnection.checkConnection();
+    let input = listings.generateData(startIndex);
+    startIndex += input.length;
+    // console.log('🔥startIndex', startIndex)
+    await insertListingData(input);
+  }
+}
+
+indexData();
+

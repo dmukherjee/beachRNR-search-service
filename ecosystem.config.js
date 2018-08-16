fdcf98135c2959c3567398bc95d19fa777bc7c02
Debalina <mukherjee.debalina@gmail.com>
2018-08-16 10:58:22 -0700

    (chore) add aws config

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [{
+    name: 'search-service',
+    script: './index.js'
+  }],
+  deploy: {
+    production: {
+      user: 'ubuntu',
+      host: 'ec2-13-57-228-175.us-west-1.compute.amazonaws.com',
+      key: '~/.ssh/search-service.pem',
+      ref: 'origin/master',
+      repo: 'git@github.com:TowerofGiraffes/beachRNR-search-service.git',
+      path: '/home/ubuntu/beachRNR-search-service',
+      'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+    }
+  }
+};

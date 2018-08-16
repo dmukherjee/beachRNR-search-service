1160f0a55f78abb601da5bc7306ffda599fa2a43
Debalina <mukherjee.debalina@gmail.com>
2018-08-16 14:58:34 -0700

    update config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [{
-    name: 'search-service',
-    cwd: 'beachrnr-search-service/src/server',
+    name: 'beachRNR-search-service',
+    cwd: '/src/server',
     script: './index.js'
   }],
   deploy: {
@@ -10,7 +10,7 @@ module.exports = {
       host: 'ec2-13-57-228-175.us-west-1.compute.amazonaws.com',
       key: '~/.ssh/search-service.pem',
       ref: 'origin/master',
-      repo: 'git@github.com:TowerofGiraffes/beachRNR-search-service.git',
+      repo: 'git@github.com:dmukherjee/beachRNR-search-service.git',
       path: '/home/ubuntu/beachRNR-search-service',
       'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
     }

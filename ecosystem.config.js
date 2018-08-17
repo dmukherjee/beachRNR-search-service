084bff8519eb02505f97167b287509a708aaadd0
Debalina <mukherjee.debalina@gmail.com>
2018-08-16 23:15:54 -0700

    update config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,14 +1,15 @@
+require('dotenv').config();
+
 module.exports = {
   apps: [{
     name: 'beachRNR-search-service',
-    cwd: '/src/server',
-    script: './index.js'
+    script: './src/server/index.js'
   }],
   deploy: {
     production: {
       user: 'ubuntu',
-      host: 'ec2-13-57-228-175.us-west-1.compute.amazonaws.com',
-      key: '~/.ssh/search-service.pem',
+      host: process.env.AWS_HOST,
+      key: '~/.ssh/beachRNR-search-service.pem',
       ref: 'origin/master',
       repo: 'git@github.com:dmukherjee/beachRNR-search-service.git',
       path: '/home/ubuntu/beachRNR-search-service',
@@ -16,4 +17,3 @@ module.exports = {
     }
   }
 };
-

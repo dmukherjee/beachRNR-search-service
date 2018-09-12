480dc1976f17cc72f211b0bdde6c35be96a47359
Debalina <mukherjee.debalina@gmail.com>
2018-09-11 18:50:36 -0700

    (feat) update index.js to accept argument

--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,7 @@
 require('newrelic');
 const app = require('./app');
+const args = process.argv.slice(2);
 
-const PORT = 3001;
+const PORT = args[0] || 3001;
 
 app.listen(PORT, () => console.log(`listening on port ${PORT}`));
